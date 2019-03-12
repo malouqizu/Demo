8ea02c110280aaea196c1239c24796a3d9f97a16
malouqizu <1085319359@qq.com>
2019-03-12 10:12:45 +0800

    Update Resquests.py

--- Resquests.py
+++ Resquests.py
@@ -20,6 +20,13 @@ rp=requests.post('http://10.33.108.46:12243/api/1014120001s/search', headers=hea
 #indent=4 将字符串打印成json格式
 print(json.dumps(rp.json(),ensure_ascii=False,indent=4))
 
+# 接口直接带参数的post请求
+headers={'token': '1011120001s'}
+data={"query":"","qfs":[],"filters":{"and":[{"or":[{"field":"cityCode","action":"match","value":"110000"}]},{"or":[{"field":"hdicResblockId","action":"match","value":"1110726255637426"}]}]},"fls":"","sorts":[],"page":0,"size":3,"hls":[],"aggregations":[],"spatial":[],"route":"110000","from":"search_result"}
+rp=requests.post('http://10.33.108.46:12243/api/1011120001s/search?id=1215889845', headers=headers, data=json.dumps(data, ensure_ascii=False))
+#indent=4 将字符串打印成json格式
+print(json.dumps(rp.json(),ensure_ascii=False,indent=4))
+
 
 
 
